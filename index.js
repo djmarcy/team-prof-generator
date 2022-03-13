601b4a689703be09caaa363630d5a80f798a71d7
djmarcy <dj_marcy@hotmail.com>
2022-03-13 17:51:13 -0600

    it's ugly, but I can generate my team

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Required applications
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const team = {
+  manager: [],
+  interns: [],
+  engineers: [],
+}
 
 // TODO: Create an array of questions for user input
 const managerQuestions = [
@@ -30,51 +38,147 @@ const managerQuestions = [
     message: "What type of employee would you like to input next?",
     choices: ["Engineer", "Intern"],
   },
-];
+]
 
-// TODO: Create a function to write README file
-function writeToFile(response) {
-  // Return HTML page
-  return ``;
-}
+const internQuestions = [
+  {
+    type: "input",
+    name: "internName",
+    message: "Intern Name:",
+  },
+  {
+    type: "input",
+    name: "internID",
+    message: "Intern ID:",
+  },
+  {
+    type: "input",
+    name: "internEmail",
+    message: "Email Address:",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "School:",
+  },
+  // Determine what to do next
+  {
+    type: "list",
+    name: "addMore",
+    message: "What type of employee would you like to input next?",
+    choices: ["Engineer", "Intern", "Finished Entering Team"],
+  }]
 
-// TODO: Create a function to initialize app
+  const engineerQuestions = [
+    {
+      type: "input",
+      name: "engineerName",
+      message: "Engineer Name:",
+    },
+    {
+      type: "input",
+      name: "engineerID",
+      message: "Engineer ID:",
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "Email Address:",
+    },
+    {
+      type: "input",
+      name: "engineerGithub",
+      message: "What is the engineer's Github profile?",
+    },
+    {
+      type: "list",
+      name: "addMore",
+      message: "What type of employee would you like to input next?",
+      choices: ["Engineer", "Intern", "Finished Entering Team"],
+    },
+  ]
+
 function init() {
-  inquirer
-    .prompt(managerQuestions)
-    .then((answers) => {
-      console.log(answers.newEmployee);
-      switch (answers.newEmployee) {
-        case "Engineer":
-          engineerQuestions();
-          break;
+inquirer
+.prompt(managerQuestions)
+  .then((answers) => {
+    
+    team.manager.push(new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOffice))
 
-        case "Intern":
-          internQuestions();
-          break;
-      }
-    })
-    .then((data) => {
-      const readme = writeToFile(data);
-      console.log(readme);
+    switch (answers.newEmployee) {
+      case "Engineer":
+        addEngineer();
+        break;
 
-      fs.writeFile("./output/team-profile.html", readme, (err) =>
-        err ? console.log(err) : console.log("Readme Successfully Generated")
-      );
-    });
-}
+      case "Intern":
+        addIntern();
+        break;
+    }
+  })
+};
 
 // Intern Questions function
-function internQuestions() {
-  // Inquirer prompts go here
-  //
+function addIntern() {
+  inquirer
+    .prompt(internQuestions)
+      .then((answers) => {
+        team.interns.push(new Intern(answers.internName, answers.internID, answers.internEmail, answers.school))
+
+        switch (answers.addMore) {
+          case "Engineer":
+            addEngineer();
+            break;
+    
+          case "Intern":
+            addIntern();
+            break;
+
+          case "Finished Entering Team":
+            console.log(team)
+            generateHTML();
+            break;
+        }
+      })
 }
 
 // Engineer Questions function
-function engineerQuestions() {
-  // Inquirer prompts go here
-  //
+function addEngineer() {
+  inquirer
+    .prompt(engineerQuestions)
+        .then((answers) => {
+          team.engineers.push(new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub))
+
+          switch (answers.addMore) {
+            case "Engineer":
+              addEngineer();
+              break;
+      
+            case "Intern":
+              addIntern();
+              break;
+  
+            case "Finished Entering Team":
+              console.log(team)
+              generateHTML();
+              break;
+          }
+        })
 }
+
+// TODO: Create a function to write README file
+function generateHTML() {
+  // Return HTML page
+  console.log(team)
+  return ``;
+}
+
+// Write to HTML
+      // const readme = writeToFile(data);
+      // console.log(readme);
+
+      // fs.writeFile("./output/team-profile.html", readme, (err) =>
+      //   err ? console.log(err) : console.log("Readme Successfully Generated")
+      // );
 
 // Function call to initialize app
 init();
