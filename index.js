25ae4ccb641e42ce43c3ecde0a6e6e0340254f4b
djmarcy <dj_marcy@hotmail.com>
2022-03-13 20:34:44 -0600

    I'm not sure what's wrong with it, but i don't have more time tonight to debug

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const team = {
   manager: [],
   interns: [],
   engineers: [],
-}
+};
 
 function generateHTML() {
-
   const content = `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -22,10 +21,10 @@ function generateHTML() {
   </head>
   <body>
   <h1>Team Profile Generator</h1>
-  <hr />`
+  <hr />`;
 
   fs.writeFile("./output/team-profile.html", content, (err) =>
-  err ? console.log(err) : console.log("Readme Successfully Generated")
+    err ? console.log(err) : console.log("Readme Successfully Generated")
   );
 
   const appendManager = `<h2>Manager:</h2>
@@ -33,53 +32,65 @@ function generateHTML() {
   <p><b>Manager ID:</b> ${team.manager.id}</p>
   <p><b>Manager Email:</b> ${team.manager.email}</p>
   <p><b>Manager Office:</b> ${team.manager.officeNum}</p>
-  <hr />`
+  <hr />`;
 
   fs.appendFile("./output/team-profile.html", appendManager, (err) =>
-  err ? console.log(err) : console.log("Manager added successfully!")
+    err ? console.log(err) : console.log("Manager added successfully!")
   );
 
-  const appendIntern = () => {
-      return `<section class="append-interns">` + team.interns.forEach(element => {
-          `<div class="intern-card">
+  appendEngineer();
+  fs.appendFile("./output/team-profile.html", appendEngineer(), (err) =>
+    err ? console.log(err) : console.log("Engineers added successfully!")
+  );
+
+  appendIntern();
+  fs.appendFile("./output/team-profile.html", appendIntern(), (err) =>
+    err ? console.log(err) : console.log("Interns added successfully!")
+  );
+
+  endContent();
+  fs.appendFile("./output/team-profile.html", endContent(), (err) =>
+    err ? console.log(err) : console.log("Team profile page ready!")
+  );
+
+  console.log(team);
+}
+
+function appendIntern() {
+  return (
+    `<section class="append-interns">` +
+    team.interns.forEach((element) => {
+      `<div class="intern-card">
           <p><b>Intern Name:</b> ${team.interns.name}</p>
           <p><b>Intern ID:</b> ${team.interns.id}</p>
           <p><b>Intern Email:</b> ${team.interns.email}</p>
           <p><b>Intern School:</b> ${team.interns.school}</p>
-          </div>` + `</section>
-          <hr />` 
-      });
-  }
-
-  fs.appendFile("./output/team-profile.html", appendIntern(), (err) =>
-  err ? console.log(err) : console.log("Interns added successfully!")
+          </div>` +
+        `</section>
+          <hr />`;
+    })
   );
+}
 
-  const appendEngineer = () => {
-      return `<section class="append-engineers">` + team.engineers.forEach(element => {
-          `<div class="intern-card">
+function appendEngineer() {
+  return (
+    `<section class="append-engineers">` +
+    team.engineers.forEach((element) => {
+      `<div class="intern-card">
           <p><b>Intern Name:</b> ${team.engineers.name}</p>
           <p><b>Intern ID:</b> ${team.engineers.id}</p>
           <p><b>Intern Email:</b> ${team.engineers.email}</p>
           <p><b>Engineer Github:</b> <a href="${team.engineers.github}">${team.engineers.github}</a></p>
-          </div>` + `</section>
-          <hr />`
-      });
-  }
-
-  fs.appendFile("./output/team-profile.html", appendEngineer(), (err) =>
-  err ? console.log(err) : console.log("Engineers added successfully!")
+          </div>` +
+        `</section>
+          <hr />`;
+    })
   );
+}
 
-  const htmlEnd = `</body>
-  <footer>&copy Marcy Web Dev 2022</footer>`
-
-  fs.appendFile("./output/team-profile.html", htmlEnd, (err) =>
-  err ? console.log(err) : console.log("Team profile page ready!")
-  );
-
-  console.log(team)
-
+function endContent() {
+  return `</body>
+  <footer>&copy Marcy Web Dev 2022</footer>`;
 }
 
 // TODO: Create an array of questions for user input
@@ -110,7 +121,7 @@ const managerQuestions = [
     message: "What type of employee would you like to input next?",
     choices: ["Engineer", "Intern"],
   },
-]
+];
 
 const internQuestions = [
   {
@@ -139,43 +150,43 @@ const internQuestions = [
     name: "addMore",
     message: "What type of employee would you like to input next?",
     choices: ["Engineer", "Intern", "Finished Entering Team"],
-  }]
+  },
+];
 
-  const engineerQuestions = [
-    {
-      type: "input",
-      name: "name",
-      message: "Engineer Name:",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Engineer ID:",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Email Address:",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is the engineer's Github profile?",
-    },
-    {
-      type: "list",
-      name: "addMore",
-      message: "What type of employee would you like to input next?",
-      choices: ["Engineer", "Intern", "Finished Entering Team"],
-    },
-  ]
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Engineer Name:",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Engineer ID:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email Address:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's Github profile?",
+  },
+  {
+    type: "list",
+    name: "addMore",
+    message: "What type of employee would you like to input next?",
+    choices: ["Engineer", "Intern", "Finished Entering Team"],
+  },
+];
 
 function init() {
-inquirer
-.prompt(managerQuestions)
-  .then((answers) => {
-    
-    team.manager.push(new Manager(answers.name, answers.id, answers.email, answers.officeNum))
+  inquirer.prompt(managerQuestions).then((answers) => {
+    team.manager.push(
+      new Manager(answers.name, answers.id, answers.email, answers.officeNum)
+    );
 
     switch (answers.newEmployee) {
       case "Engineer":
@@ -186,55 +197,55 @@ inquirer
         addIntern();
         break;
     }
-  })
-};
+  });
+}
 
 // Intern Questions function
 function addIntern() {
-  inquirer
-    .prompt(internQuestions)
-      .then((answers) => {
-        team.interns.push(new Intern(answers.name, answers.id, answers.email, answers.school))
+  inquirer.prompt(internQuestions).then((answers) => {
+    team.interns.push(
+      new Intern(answers.name, answers.id, answers.email, answers.school)
+    );
 
-        switch (answers.addMore) {
-          case "Engineer":
-            addEngineer();
-            break;
-    
-          case "Intern":
-            addIntern();
-            break;
+    switch (answers.addMore) {
+      case "Engineer":
+        addEngineer();
+        break;
 
-          case "Finished Entering Team":
-            console.log(team)
-            generateHTML();
-            break;
-        }
-      })
+      case "Intern":
+        addIntern();
+        break;
+
+      case "Finished Entering Team":
+        console.log(team);
+        generateHTML();
+        break;
+    }
+  });
 }
 
 // Engineer Questions function
 function addEngineer() {
-  inquirer
-    .prompt(engineerQuestions)
-        .then((answers) => {
-          team.engineers.push(new Engineer(answers.name, answers.id, answers.email, answers.github))
+  inquirer.prompt(engineerQuestions).then((answers) => {
+    team.engineers.push(
+      new Engineer(answers.name, answers.id, answers.email, answers.github)
+    );
 
-          switch (answers.addMore) {
-            case "Engineer":
-              addEngineer();
-              break;
-      
-            case "Intern":
-              addIntern();
-              break;
-  
-            case "Finished Entering Team":
-              console.log(team)
-              generateHTML();
-              break;
-          }
-        })
+    switch (answers.addMore) {
+      case "Engineer":
+        addEngineer();
+        break;
+
+      case "Intern":
+        addIntern();
+        break;
+
+      case "Finished Entering Team":
+        console.log(team);
+        generateHTML();
+        break;
+    }
+  });
 }
 
 // Function call to initialize app
